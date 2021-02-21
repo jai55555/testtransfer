056fce7f87dac3b1a59d4f70e516241fc0c65186
jai55555 <jai_cp25@yahoo.com>
2021-02-21 13:36:27 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -41,7 +41,7 @@ if (!is_null($events['events'])) {
 			$mysqltext = "Image";
 			$mysqltype = "1";
 			
-			$res = notify_message('https://api.line.me/v2/bot/message/'.$event['message']['id'].'/content',$token);
+			$res = notify_message('https://api-data.line.me/v2/bot/message/'.$event['message']['id'].'/content',$token);
 			//$text = $text . ' https://api.line.me/v2/bot/message/'.$event['message']['id'].'/content';
 		}
 
